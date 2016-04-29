c0e6e630943a0b0defdfe167c179f890b8049671
ixxra <garciamx@gmail.com>
2016-04-29 10:23:44 -0500

    renamed dist to public

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,19 @@
 var gulp = require('gulp');
 
-
-
 gulp.task('compileMarkup', function() {
     var srcGenerator = require('./lib/markupCompiler.js');
-    srcGenerator().pipe(gulp.dest('dist'));
+    srcGenerator().pipe(gulp.dest('public'));
 });
 
 gulp.task('copyRevealAssets', function (){
     gulp.src('node_modules/reveal.js/css/**/*.css')
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('public/css'));
     gulp.src('node_modules/reveal.js/js/*.js')
-        .pipe(gulp.dest('dist/js'));
+        .pipe(gulp.dest('public/js'));
     gulp.src('node_modules/reveal.js/lib/**')
-        .pipe(gulp.dest('dist/lib'));
+        .pipe(gulp.dest('public/lib'));
     gulp.src('node_modules/reveal.js/plugin/**')
-        .pipe(gulp.dest('dist/plugin'));    
+        .pipe(gulp.dest('public/plugin'));    
 });
 
 gulp.task('serve', function () {
